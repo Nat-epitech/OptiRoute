f8a737bad466bf2d0d3324cdaf6e6740f2830bbb
Nathan DP <nathanterre@gmail.com>
2026-05-08 00:07:37 +0200

    OptiRoute initialisation

--- backend/src/main/java/com/optiroute/backend/BackendApplication.java
+++ backend/src/main/java/com/optiroute/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.simpletms.backend;
+package com.optiroute.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- frontend/src/views/Planning.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Planning</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
