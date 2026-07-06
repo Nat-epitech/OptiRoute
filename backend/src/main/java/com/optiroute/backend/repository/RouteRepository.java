00ee09591969919c3f99756b5f0f34529201ce42
Nathan DP <nathan.dipalma74@gmail.com>
2026-07-07 00:28:39 +0200

    Remove route services

--- backend/src/main/java/com/optiroute/backend/repository/RouteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.optiroute.backend.repository;
-
-import com.optiroute.backend.entity.RouteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<RouteEntity, Long> {
-}
